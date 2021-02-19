fde0152094ff22451f228c0c1a80ab9adb7c1639
Ian Marshall <imarshall14@gmail.com>
2021-02-19 17:26:51 -0500

    commit tree f4561d826b14d12f7c7134b4b3de326e1f5c6b1a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,15 @@
+require 'pry'
+
 class StudentsController < ApplicationController
   
   def index
     @students = Student.all
   end
 
-  # def new
-
-  # end
-
   def create
+    # binding.pry
+    @student = Student.create(first_name: params[:student][:first_name],last_name: params[:student][:last_name])
+    redirect_to students_path
 
   end
 
